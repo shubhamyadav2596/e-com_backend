c419547d845c9e61cdaa8b362d0519f663b03078
progammerBoy <sy8692935@gmail.com>
2026-06-28 16:55:54 +0530

    fix minor error

--- server.js
+++ server.js
@@ -64,6 +64,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const serverless = require('serverless-http');
 
-
-module.exports = app;
+module.exports = serverless(app);
