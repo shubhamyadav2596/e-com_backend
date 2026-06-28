2c5cc4fa7af0d38caa34ae3b3236fdf4fca2f0c8
progammerBoy <sy8692935@gmail.com>
2026-06-28 18:44:55 +0530

    cloudinary issue fixed

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,10 +1,15 @@
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
+if (!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET) {
+  throw new Error('Missing Cloudinary env variables: CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET');
+}
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,9 +1,8 @@
 const admin = (req, res, next) => {
   if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res.status(401).json({ message: 'Not authorized as an admin' });
+    return next();
   }
+  res.status(403).json({ message: 'Forbidden: admin access required' });
 };
 
 module.exports = { admin };
